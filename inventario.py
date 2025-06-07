133301748993b5e6318bae7f4f9dc55d7cd6c440
natanramos252 <natanramos@gmail.com>
2025-06-07 17:52:05 -0300

    printa mais esta sem formatação estou encrementando opção para indicar um caminho pro arquivo inventario

--- inventario.py
+++ inventario.py
@@ -7,11 +7,16 @@ def enc_iddisp(d_inventario):
         chave = max(d_inventario.keys())+1
     return chave
 
+def caminho():
+    
+    caminho = imput('informe o caminho: ')
+    return caminho
+    
 def le_inventario():
     
     try:
         d_inventario ={}
-        with open('inventario.cvs','r') as inventario:
+        with open('inventario.csv','r') as inventario:
             for linha in inventario:
                 chave, nome, qt, preco, impor = linha.rstrip().split(';')
                 chave = int(chave)
@@ -26,8 +31,13 @@ def le_inventario():
                 d_inventario[chave] = [nome, qt, preco, impor]
         
     except FileNotFoundError:
-        print('''Arquivo inventario.cvs não encontrado!
-adicione o arquivo ou um novo sera criado''')
+        print('''Arquivo inventario.csv não encontrado!!!! adicione o arquivo ou um novo sera criado.
+              
+              1 - para indicar um caminho
+              2 - para criar um novo arquivo''')
+        opção = int(input())
+
+        
            
     return d_inventario
                 
@@ -57,7 +67,7 @@ def apaga_iten(d_inventario, chave):
     return d_inventario
 
 def grava_inventario(d_inventario):
-    with open('inventario.cvs','w') as inventario:
+    with open('inventario.csv','w') as inventario:
         for chave, dados in d_inventario.items():
             nome = dados[0]
             qt = dados[1]
@@ -65,12 +75,19 @@ def grava_inventario(d_inventario):
             impor = dados[3]
             inventario.write(f'{chave};{nome};{qt};{preco};{impor}\n')
 
-def imp_inventario(d_inventario):
+def imp_inventario(dicionario):
     #esta dando erro de sintax
-    for chave, dados in d_inventario.items():
-        print(f'{chave}: ')
-        for item in dados:
-            print(f'{nome} | {qt}| {preco}| {impor}')
+    print('---'*20)
+    print('produtos')
+    print('---'*20)
+
+    for chave, dados in dicionario.items():
+        nome = dados[0]
+        qt = dados[1]
+        preco = dados[2]
+        impor = dados[3]
+        print(f'{chave}: {nome} | {qt}| {preco}| {impor}')
+        #print(f'{nome} | {qt}| {preco}| {impor}')
 
 
 
@@ -79,4 +96,5 @@ d_inventario = le_inventario()
 chave = enc_iddisp(d_inventario)
 #chave = ad_inventario(d_inventario, chave)
 
-print(d_inventario)
+#print(d_inventario)
+imp_inventario(d_inventario) 
